9328db4f3c1b6a6bfae5ab767ae7026f6dc2aa1f
ecalvo92 <ecalvo@crnova.com>
2024-09-26 20:44:46 -0600

    Vista de Inicio de sesión y nuevo layout

---
+++ JWeb/JWeb/Controllers/LoginController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace JWeb.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult InicioSesion()
+        {
+            return View();
+        }
+    }
+}
